f92b84e72034e6bf79a55df1b9544d633e4dea28
Crunchy.dev <150474806+Crunchyrocks1@users.noreply.github.com>
2025-01-07 15:30:12 -0600

    Update Registry.py

--- Logs/Windows/Registry.py
+++ Logs/Windows/Registry.py
@@ -2,19 +2,16 @@ import time
 import winsound
 from plyer import notification
 
-# Function to display a notification and play a beep
 def notify():
-    # Display the notification
     notification.notify(
-        title='Notification Title',
-        message='This is your notification message!',
-        timeout=5  # Time in seconds for the notification to stay on screen
+        title='ANTI CHEAT ENABLED',
+        message='Nothing to see here...',
+        timeout=8  
     )
 
-    # Play a beep sound
-    winsound.Beep(1000, 500)  # Frequency: 1000Hz, Duration: 500ms
+
+    winsound.Beep(1000, 750)  
 
 if __name__ == "__main__":
-    # Trigger the notification and beep
     notify()
-    time.sleep(2)  # Wait for 2 seconds before exiting
+    time.sleep(2) 
