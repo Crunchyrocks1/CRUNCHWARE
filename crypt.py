abb180c2b247f9d3b9a84811f6fa42ab4f566ef1
Crunchy.dev <150474806+Crunchyrocks1@users.noreply.github.com>
2025-01-10 13:00:20 -0600

    Add files via upload

---
+++ crypt.py
@@ -0,0 +1,13 @@
+# crypt - basic way of changing func names 
+# to make it either easier to harder to read
+# Not used to hide code but simplify 
+
+
+import os , sys , ctypes
+
+
+def log(message):
+    print(message)
+
+
+
