eaabc8c9128704117527294522f0c2a87ebc15ea
Crunchy.dev <150474806+Crunchyrocks1@users.noreply.github.com>
2025-01-10 12:48:51 -0600

    Add files via upload

---
+++ pyt/imports.py
@@ -0,0 +1,17 @@
+from pyt.crypt import log
+from pyt import init, Fore
+from pyt.CtypeClick import *
+
+from pyt.messages import *
+
+
+user32 = ctypes.windll.user32
+gdi32 = ctypes.windll.gdi32
+kernel32 = ctypes.windll.kernel32
+
+
+
+
+
+
+# all imports 
---
+++ pyt/messages.py
@@ -0,0 +1,9 @@
+
+
+# Checking For Updates
+Message1 = "Checking For Updates..."
+Message2 = "Updated Sucessfully!"
+Message3 = "Update Failed..."
+
+
+Error1 = "Failed..."
